f5a6f9eb00c28c54b34b380357a078ec43da7731
Karisson Sorrigotti <sorrigotti@yandex.com>
2023-10-20 12:49:34 +0100

    java error

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-//pegar a tag img 
-const img = document.querySelector("#profile img")
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
 
-//substituir a img
+  //substituir a img
   if (html.classList.contains("light")) {
-//se tiver a light mode, adicionar a img light    
- img.setAttributte("src", "./assets/avatar-light.png")
-} else {
-//se tiver sem light mode, manter a imagem normal
-img.setAttributte("src", "./assets/avatar.png")
-}
-
+    //se tiver a light mode, adicionar a img light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
